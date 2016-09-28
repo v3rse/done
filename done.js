bf8c0f2b522fbedbb259628bf453ec591e7faf21
Nana Adane <nanaofosuheneadane@gmail.com>
2016-09-28 21:09:50 +0000

    Add multiple list display option 'all|done'

--- done.js
+++ done.js
@@ -47,7 +47,7 @@ function setData(data) {
 
 //Displays usage
 function usage() {
-	console.log("Usage: done [add|check|delete|help] [task]");
+	console.log("Usage: done [add|check|delete|help|list [all|done]] [task]");
 	console.log("`task` is only a string when using `add` and a number\nfor all other commands.");
 	console.log("Using the `done` without arguments lists all tasks");
 }
@@ -124,30 +124,47 @@ function del(task) {
 
 
 
+//Lists all pending tasks
+function list(){
+        var data = getData();
+
+	if (data.uncompleted.length) {
+		printUncompleted(data);	
+	} else {
+		displayError("No tasks added!!");
+	}
+}
+
+
+
+//Lists all pending tasks
+function listCompleted(){
+        var data = getData();
+
+	if (data.completed.length) {
+		printCompleted(data);	
+	} else {
+		displayError("No tasks added!!");
+	}
+}
+
+
+
+//Lists all completed tasks
+
 
 //Lists all tasks
-function list() {
+function listAll() {
 
 	//data
 	var data = getData();
 
 	if (data.uncompleted.length || data.completed.length) {
+		
+		printUncompleted(data);
+		console.log("\n");	
+		printCompleted(data);
 
-		if (data.uncompleted.length) {
-			//print the uncompleted list. using ANSI colors and formating
-			console.log(chroma.underline.lyellow("Uncompleted Task list:"));
-			data.uncompleted.forEach(function (task, index) {
-				console.log(chroma.lyellow(index + 1 + "."), task.task + "\t" + chroma.italics.bgblue("Added " + moment(task.dateCreated).fromNow()));
-			});
-		}
-		if (data.completed.length) {
-			//print the uncompleted list. using ANSI colors and formating
-			console.log("\n");
-			console.log(chroma.underline.lyellow("Completed Task list:"));
-			data.completed.forEach(function (task, index) {
-				console.log(chroma.strikethrough.lgreen(index + 1 + ". " + task.task),chroma.italics.bgblue("\tCompleted " + moment(task.dateCompleted).fromNow()));
-			});
-		}
 	} else {
 		displayError("No tasks added!!");
 	}
@@ -155,11 +172,35 @@ function list() {
 }
 
 //Utils
+
+//Formating for errors
 function displayError(string){
 	console.log(chroma.bgred(chroma.black(string)));
 }
 
+//Prints pending tasks
+function printUncompleted(data){
+	if (data.uncompleted.length) {
+		//print the uncompleted list. using ANSI colors and formating
+		console.log(chroma.underline.lyellow("Uncompleted Task list:"));
+		data.uncompleted.forEach(function (task, index) {
+			console.log(chroma.lyellow(index + 1 + "."), task.task + "\t" + chroma.italics.bgblue("Added " + moment(task.dateCreated).fromNow()));
+		});
+	}
+}
 
+
+
+//Prints completed tasks
+function printCompleted(data){
+	if (data.completed.length) {
+				//print the uncompleted list. using ANSI colors and formating
+				console.log(chroma.underline.lyellow("Completed Task list:"));
+				data.completed.forEach(function (task, index) {
+					console.log(chroma.strikethrough.lgreen(index + 1 + ". " + task.task),chroma.italics.bgblue("\tCompleted " + moment(task.dateCompleted).fromNow()));
+				});
+			}
+}
 
 //Entry point
 var command = process.argv[2];
@@ -179,6 +220,15 @@ switch (command) {
 		break;
 	case "help":
 		usage();
+		break;
+	case "list":
+		if(argument == "all"){
+			listAll();
+		}else if(argument == "done"){
+			listCompleted();
+		}else{
+			list();
+		}	
 		break;
 	case undefined:
 		list();
